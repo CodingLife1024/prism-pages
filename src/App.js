93a6c041e4ec353735a5614c82c8bdb47f91f53a
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-18 03:30:36 +0530

    rte

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import Article from './Article';
 
 function App() {
-  // const githubLink = 'https://api.github.com/repos/CodingLife1024/blog-content/contents/2024-01-14::02:16.md';
+  const githubLink = 'https://api.github.com/repos/CodingLife1024/blog-content/contents/2024-01-14::02:16.md';
 
   return (
     <div className="App">
@@ -15,7 +15,7 @@ function App() {
         {/* <PageLoader githubLink={githubLink}/> */}
         <Link to="/Article">Go to Article</Link>
         <Routes>
-          <Route path="/Article" element={<Article />} />
+          <Route path="/Articl" element={<Article githubLink={githubLink} />} />
         </Routes>
       </header>
     </div>
--- src/Article/index.jsx
+++ src/Article/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import PageLoader from '../PageLoader/PageLoader'
 
-const Article = () => {
+const Article = ( {githubLink} ) => {
   return (
     <><div>Article</div>
-    <PageLoader githubLink="https://api.github.com/repos/CodingLife1024/blog-content/contents/2024-01-14::02:16.md" />
+    <PageLoader githubLink={githubLink} />
     </>
   )
 }
