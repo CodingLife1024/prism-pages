4fc6125a5ca7e49a171e580b0a7792c69053d36a
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-19 22:10:20 +0530

    sdgyeor

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-// src/App.js
-import React from 'react';
-import { Routes, Route, Link } from "react-router-dom";
-// import GitHubFileLoader from './FileLoader/GitHubFileLoader';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import GitHubFileLoader from './FileLoader/GitHubFileLoader';
 import PageLoader from './PageLoader/PageLoader';
-import Article from './Article';
 
 function App() {
-  const githubLink = 'https://api.github.com/repos/CodingLife1024/blog-content/contents/2024-01-14::02:16.md';
+  const githubApi = 'https://api.github.com/repos/CodingLife1024/blog-content/contents/';
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    // Fetch the list of files from the GitHub repository
+    fetch(githubApi)
+      .then((response) => response.json())
+      .then((data) => {
+        // Filter out non-file items (e.g., directories)
+        const fileItems = data.filter((item) => item.type === 'file');
+        setFiles(fileItems);
+      })
+      .catch((error) => console.error('Error fetching files:', error));
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <GitHubFileLoader githubLink={githubLink} /> */}
-        <PageLoader githubLink={githubLink}/>
-        <Link to="/Article">Go to Article</Link>
+        {/* Generate links dynamically based on the files */}
+        {files.map((file) => (
+          <Link key={file.name} to={`/${file.name}`}>
+            {file.name}
+          </Link>
+        ))}
+
+        {/* Routes for each file */}
         <Routes>
-          <Route path="/Articl" element={<Article githubLink={githubLink} />} />
+          {files.map((file) => (
+            <Route
+              key={file.name}
+              path={`/${file.name}`}
+              element={<PageLoader githubLink={`${githubApi}${file.name}`} />}
+            />
+          ))}
         </Routes>
       </header>
     </div>
