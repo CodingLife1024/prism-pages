390e15ed85b9e25f95ff3ce5580ba2de57950c38
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-21 02:41:05 +0530

    hgv

--- src/scenes/Topbar/index.jsx
+++ src/scenes/Topbar/index.jsx
@@ -14,8 +14,12 @@ const TopBar = () => {
         <FaFacebook style={style} />
         <FaTwitter style={style} />
       </div>
-      <div className={styles.intro}><p>Just a Computer Science student.</p> </div>
-      <div className={styles.interests}>I am interested in....</div>
+      <div className={styles.intro}>
+        <p>Hi, I am Riddhi, a pre-final year Computer Science undergraduate student at IIT Delhi.My interests are AI and ML algorithms, Web and App Development and Game Theory.</p>
+      </div>
+      <div className={styles.interests}>
+        I am a part of <a href="https://facciitd.netlify.app/" target="_blank">Azure</a> (IITD Art Club) and <a href="https://indradhanuiitd.in/" target="_blank">Indradhanu</a> (Queer Collective).
+      </div>
     </div>
   )
 }
