387bbe56f566b7a0b8a3343203e1c334014edca4
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-21 01:46:32 +0530

    uebgwiu

--- src/scenes/Categories/index.jsx
+++ src/scenes/Categories/index.jsx
@@ -3,8 +3,13 @@ import styles from "./category.module.css"
 
 const Category = () => {
   return (
-    <div className=''>Category
-
+    <div className={styles.category}>
+      <div className={styles.title}>Categories</div>
+      <div className={styles.technology}>technology</div>
+      <div className={styles.experiences}>experiences</div>
+      <div className={styles.books}>books</div>
+      <div className={styles.films}>films</div>
+      <div className={styles.lgbt}>lgbt</div>
     </div>
   )
 }
--- src/scenes/Dashboard/index.jsx
+++ src/scenes/Dashboard/index.jsx
@@ -2,15 +2,35 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import PageLoader from '../../PageLoader/PageLoader'
 import TitleList from '../TitleList'
+import styles from './dashboard.module.css'
 
 const Dashboard = () => {
   return (
-    <div>
+    <div className={styles.dashboard}>
       {/* <Routes>
         <Route path="/" element={<TitleList />} />
         <Route path="/ttt" element={<PageLoader githubLink="https://api.github.com/repos/CodingLife1024/blog-content/contents/README.md" />} />
       </Routes> */}
-      Dashboard
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
+      <div className={styles.topics}>Ode to Frankenstein</div>
     </div>
   )
 }
