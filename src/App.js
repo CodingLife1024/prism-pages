b1f47edf513d7af0880e3a6f5a1af3221a735da5
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-24 13:52:25 +0530

    wegiweu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // App.js
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import TitleList from './scenes/TitleList/index';
 import PageLoader from './PageLoader/PageLoader';
 import TopBar from './scenes/Topbar';
 import SideBar from './scenes/Sidebar';
@@ -21,6 +20,7 @@ function App() {
           <SideBar />
         </div>
       </div>
+
     </>
   );
 }
