3414e425dd79767194d87b04f6b903abd8285751
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-25 00:45:33 +0530

    rejhbg

--- src/scenes/Sidebar/index.jsx
+++ src/scenes/Sidebar/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styles from './sidebar.module.css'
+import { Link } from 'react-router-dom'
 
 const SideBar = () => {
   return (
@@ -7,7 +8,7 @@ const SideBar = () => {
       <div className={styles.texts}>My Website</div>
       <div className={styles.texts}>Books</div>
       <div className={styles.texts}>Movies</div>
-      <div className={styles.texts}>About</div>
+      <Link to="/about" className={styles.texts}>About</Link>
     </div>
   )
 }
