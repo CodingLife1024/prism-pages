e49a0561a538ae2ac988c438c765fecc4448b250
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-25 00:43:24 +0530

    yuguy

--- src/App.js
+++ src/App.js
@@ -12,14 +12,23 @@ function App() {
   return (
     <>
       {/* <PageLoader githubLink="https://api.github.com/repos/CodingLife1024/blog-content/contents/README.md" /> */}
-      <div className='full'>
-        <TopBar />
-        <div className="main-container">
-          <Category />
-          <Dashboard />
-          <SideBar />
-        </div>
-      </div>
+
+      <Routes>
+        <Route path="/" element={
+          <>
+          <div className='full'>
+            <TopBar />
+            <div className="main-container">
+              <Category />
+              <Dashboard />
+              <SideBar />
+            </div>
+          </div>
+          </>
+        } />
+
+        <Route path="/about" element={<PageLoader githubLink="https://api.github.com/repos/CodingLife1024/blog-content/contents/pride-a-history.md" />} />
+      </Routes>
 
     </>
   );
