2dc78103ac5b27a93e3f50072ea18a8d69ba9839
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-27 03:34:14 +0530

    tft

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
           </>
         } />
 
-        <Route path="/about" element={<About/>} />
+        <Route path="/about" element={<About />} />
 
         <Route path="/pride-a-history.md" element={<PageLoader githubLink="https://api.github.com/repos/CodingLife1024/blog-content/contents/pride-a-history.md" />} />
 
--- src/scenes/About/index.jsx
+++ src/scenes/About/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div>
+      <h2>About Us</h2>
+      <p>
+        Welcome to our website! We are passionate about sharing knowledge and experiences.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et justo ut nibh congue semper.
+        Proin elementum tempus nulla, id pharetra risus cursus id. Quisque vel urna at libero euismod
+        fringilla sit amet eget orci.
+      </p>
+      <ul>
+        <li>Our Mission: To provide valuable content to our readers.</li>
+        <li>Founded in: 2020</li>
+        <li>Contact Us: info@example.com</li>
+      </ul>
+      <p>
+        Thank you for visiting our website. Feel free to explore and enjoy the content we have to offer!
+      </p>
+    </div>
+  );
+};
 
-export default About
+export default About;
