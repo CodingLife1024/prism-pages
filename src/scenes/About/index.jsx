84d909dc06d2283b5353e665f8e6e299058a57af
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-27 03:38:11 +0530

    gv

--- src/scenes/About/index.jsx
+++ src/scenes/About/index.jsx
@@ -1,25 +1,9 @@
 import React from 'react';
-
+import styles from './about.module.css'
 const About = () => {
   return (
     <div>
-      <h2>About Us</h2>
-      <p>
-        Welcome to our website! We are passionate about sharing knowledge and experiences.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et justo ut nibh congue semper.
-        Proin elementum tempus nulla, id pharetra risus cursus id. Quisque vel urna at libero euismod
-        fringilla sit amet eget orci.
-      </p>
-      <ul>
-        <li>Our Mission: To provide valuable content to our readers.</li>
-        <li>Founded in: 2020</li>
-        <li>Contact Us: info@example.com</li>
-      </ul>
-      <p>
-        Thank you for visiting our website. Feel free to explore and enjoy the content we have to offer!
-      </p>
+      About Us
     </div>
   );
 };
