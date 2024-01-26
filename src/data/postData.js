3961c1dd9f246af47c3e2edad23c36fafb782bd3
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-27 03:26:53 +0530

    gber

--- src/data/postData.js
+++ src/data/postData.js
@@ -3,6 +3,23 @@ export const postData = [
         id: 1,
         title: "Pride: A History",
         link: "pride-a-history",
+        file: "pride-a-history.md",
         category: [lgbt]
-    }
+    },
+
+    {
+        id: 2,
+        title: "The All-Nighter Chronicles: Surviving Finals Week with Tech",
+        link: "the-all-nighter-chronicles",
+        file: "the-all-nighter-chronicles.md",
+        category: [experiences]
+    },
+
+    {
+        id: 3,
+        title: "Readme",
+        link: "readme",
+        file: "README.md",
+        category: [lgbt]
+    },
 ]
