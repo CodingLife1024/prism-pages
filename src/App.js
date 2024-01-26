4b87f9858e5289013c48e7268e214f7e5446caf3
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-26 18:16:46 +0530

    uierth

--- src/App.js
+++ src/App.js
@@ -29,6 +29,8 @@ function App() {
         } />
 
         <Route path="/about" element={<About/>} />
+
+        <Route path="/pride-a-history.md" element={<PageLoader githubLink="https://api.github.com/repos/CodingLife1024/blog-content/contents/pride-a-history.md" />} />
       </Routes>
 
     </>
