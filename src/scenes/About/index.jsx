7939019dc0e63826a72fc94bfb8ceb8abd0b0a77
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-01-27 23:52:33 +0530

    yfyu

--- src/scenes/About/index.jsx
+++ src/scenes/About/index.jsx
@@ -2,8 +2,28 @@ import React from 'react';
 import styles from './about.module.css'
 const About = () => {
   return (
-    <div>
-      About Us
+    <div className={styles.main}>
+      <div className={styles.aboutme}>
+        About Me
+      </div>
+      <div className={styles.para}>
+        <p>I am Riddhi, a 3rd year student at IIT Delhi. This blog is my attempt to get out of my FOMO.</p>
+      </div>
+      <div className={styles.aboutme}>
+        About the Blog
+      </div>
+      <div className={styles.para}>
+        <p>I will be writing about my areas of interest on this blog - Books, Movies, and Technology. This blog is my digital playground, where I share my experiences, insights and adventures. Horror, history or my latest technical proect, I am here to document it all.</p>
+        <p>Join me on this journey of growth, learning, and occasional trial-and-error. Let us explore the intersection of academia and hobbies together. </p>
+        <p>Thanks for stopping by, and I hope you find something here that sparks your curiosity. </p>
+      </div>
+
+      <div className={styles.aboutme}>
+        Contact me
+      </div>
+      <div className={styles.para}>
+
+      </div>
     </div>
   );
 };
