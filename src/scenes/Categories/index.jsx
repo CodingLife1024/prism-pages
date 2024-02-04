9f47c234a866bd139f683273268240101a1174f9
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-02-05 04:44:48 +0530

    eyfuw

--- src/scenes/Categories/index.jsx
+++ src/scenes/Categories/index.jsx
@@ -10,12 +10,12 @@ const Category = ({ onCategoryClick, selectedTag }) => {
   return (
     <div className={styles.category}>
       <div className={styles.title}>Categories</div>
-      <Link to="/" onClick={() => handleClick("all")} className={`${styles.categoryLink} ${selectedTag === "all" ? styles.active : styles.default}`}>All</Link>
-      <Link to="/" onClick={() => handleClick("tech")} className={`${styles.categoryLink} ${selectedTag === "tech" ? styles.active : styles.default}`}>Technology</Link>
-      <Link to="/" onClick={() => handleClick("experiences")} className={`${styles.categoryLink} ${selectedTag === "experiences" ? styles.active : styles.default}`}>Experiences</Link>
-      <Link to="/" onClick={() => handleClick("books")} className={`${styles.categoryLink} ${selectedTag === "books" ? styles.active : styles.default}`}>Books</Link>
-      <Link to="/" onClick={() => handleClick("films")} className={`${styles.categoryLink} ${selectedTag === "films" ? styles.active : styles.default}`}>Films</Link>
-      <Link to="/" onClick={() => handleClick("lgbt")} className={`${styles.categoryLink} ${selectedTag === "lgbt" ? styles.active : styles.default}`}>LGBT</Link>
+      <Link to="/" onClick={() => handleClick("all")} className={`${styles.categoryLink} ${selectedTag === "all" ? styles.active : styles.default_all}`}>All</Link>
+      <Link to="/" onClick={() => handleClick("tech")} className={`${styles.categoryLink} ${selectedTag === "tech" ? styles.active : styles.default_tech}`}>Technology</Link>
+      <Link to="/" onClick={() => handleClick("experiences")} className={`${styles.categoryLink} ${selectedTag === "experiences" ? styles.active : styles.default_experiences}`}>Experiences</Link>
+      <Link to="/" onClick={() => handleClick("books")} className={`${styles.categoryLink} ${selectedTag === "books" ? styles.active : styles.default_books}`}>Books</Link>
+      <Link to="/" onClick={() => handleClick("films")} className={`${styles.categoryLink} ${selectedTag === "films" ? styles.active : styles.default_films}`}>Films</Link>
+      <Link to="/" onClick={() => handleClick("lgbt")} className={`${styles.categoryLink} ${selectedTag === "lgbt" ? styles.active : styles.default_lgbt}`}>LGBT</Link>
     </div>
   );
 }
