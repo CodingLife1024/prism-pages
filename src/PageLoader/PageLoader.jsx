92dbba08c3150bc72292e8900a3f3948305cfebd
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-03-04 10:37:20 +0530

    ybi

--- src/PageLoader/PageLoader.jsx
+++ src/PageLoader/PageLoader.jsx
@@ -5,7 +5,6 @@ import GitHubFileLoader from '../FileLoader/GitHubFileLoader';
 import styles from './pageloader.module.css'
 
 const PageLoader = ( {githubLink} ) => {
-  console.log('PageLoader rendered with githubLink:', githubLink);
   return (
     <div className={styles.main}>
       <GitHubFileLoader githubLink={githubLink} />
