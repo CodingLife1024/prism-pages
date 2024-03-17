4847c24e3ad3917946333afae67ca688eabd9add
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-03-18 00:51:09 +0530

    fbsd

--- src/Layout.js
+++ src/Layout.js
@@ -7,6 +7,14 @@ const Layout = ({ children }) => {
   return (
     <html lang="en">
       <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-FZG9F8BZPG"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-FZG9F8BZPG');
+      </script>
         <title>Next.js</title>
       </head>
       <body>
