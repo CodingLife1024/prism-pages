20852abc9d19b9beb9ad4522766d6934efbab79e
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-03-18 00:53:03 +0530

    rerge

--- src/Layout.js
+++ src/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
       <script async src="https://www.googletagmanager.com/gtag/js?id=G-FZG9F8BZPG"></script>
       <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+        function gtag()dataLayer.push(arguments);
         gtag('js', new Date());
 
         gtag('config', 'G-FZG9F8BZPG');
