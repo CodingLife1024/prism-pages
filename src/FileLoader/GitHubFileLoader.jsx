3759c3ee045c9313d5b698eaba6d0ae7c1b1bc52
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-08-23 01:53:50 +0530

    un[oi

--- src/FileLoader/GitHubFileLoader.jsx
+++ src/FileLoader/GitHubFileLoader.jsx
@@ -4,7 +4,7 @@ import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-// import 'katex/dist/katex.min.css';
+import 'katex/dist/katex.min.css';
 
 const GitHubFileLoader = ({ githubLink }) => {
   console.log('GitHubFileLoader rendered with githubLink:', githubLink);
