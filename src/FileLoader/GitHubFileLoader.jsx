d7c17521cc87c07e7b3da51eaaaf555ce87290de
Riddhidipta Pal <riddhidipta2003@gmail.com>
2024-12-24 07:03:45 +0530

    iugou

--- src/FileLoader/GitHubFileLoader.jsx
+++ src/FileLoader/GitHubFileLoader.jsx
@@ -41,26 +41,29 @@ const GitHubFileLoader = ({ githubLink }) => {
   }, [githubLink]);
 
   return (
-    <div style={{ maxWidth: '100%', padding: '30px' }}>
+    <div style={{ maxWidth: '100%', padding: '30px', overflowY: 'auto', maxHeight: 'calc(100vh - 60px)' }}>
       {fileContent ? (
-        <ReactMarkdown
-          className={styles.markdown}
-          remarkPlugins={[remarkGfm, remarkMath]}
-          rehypePlugins={[rehypeKatex]}
-          components={{
-            img: ({ node, ...props }) => <img {...props} style={{ width: '100%' }} alt="" />,
-            table: ({ node, ...props }) => <table {...props} style={{ borderCollapse: 'collapse', width: '100%' }} />,
-            th: ({ node, ...props }) => <th {...props} style={{ border: '1px solid #ddd', padding: '8px' }} />,
-            td: ({ node, ...props }) => <td {...props} style={{ border: '1px solid #ddd', padding: '8px' }} />,
-          }}
-        >
-          {fileContent}
-        </ReactMarkdown>
+        <div style={{ overflowY: 'auto' }}>
+          <ReactMarkdown
+            className={styles.markdown}
+            remarkPlugins={[remarkGfm, remarkMath]}
+            rehypePlugins={[rehypeKatex]}
+            components={{
+              img: ({ node, ...props }) => <img {...props} style={{ width: '100%' }} alt="" />,
+              table: ({ node, ...props }) => <table {...props} style={{ borderCollapse: 'collapse', width: '100%' }} />,
+              th: ({ node, ...props }) => <th {...props} style={{ border: '1px solid #ddd', padding: '8px' }} />,
+              td: ({ node, ...props }) => <td {...props} style={{ border: '1px solid #ddd', padding: '8px' }} />,
+            }}
+          >
+            {fileContent}
+          </ReactMarkdown>
+        </div>
       ) : (
         <p>Loading content...</p>
       )}
     </div>
   );
+
 };
 
 export default GitHubFileLoader;
